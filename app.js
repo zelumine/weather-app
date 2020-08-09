543144f6c8d197036d40709558d746c8937f4eab
Mélusine Rey <zelumine@live.fr>
2020-08-09 15:38:17 +0200

    Add elements, style and function to get current weather

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const selectList = document.getElementById('select-cities');
+const key = '39b2960a873461b5190f79a9a24fb051'; 
 const url = './cities-fr.json';
+let weatherUrl;
+let todaysDate = new Date();
 
+const selectList = document.getElementById('select-cities');
+const cityName = document.querySelector('#city-title');
+const currentWeather = document.querySelector('#current-weather');
+const currentTemp = document.querySelector('#current-temp');
+const nextDaysWeather = document.querySelector('#next-days');
+
+// Got help from https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
 fetch(url)  
   .then(  
     function(response) {  
       if (response.status !== 200) {  
-        console.warn('Looks like there was a problem. Status Code: ' + 
-          response.status);  
+        console.warn(`There was a problem. Status code: ${response.status}`);  
         return;  
       }
 
-      // Examine the text in the response  
       response.json().then(function(data) { 
         let option;
     
@@ -19,11 +26,36 @@ fetch(url)
       	  option.text = data[i].nm;
       	  option.value = data[i].nm;
       	  selectList.appendChild(option);
-    	  }    
+        }    
+        document.getElementsByTagName('option')[0].setAttribute('selected', 'true');
       });  
     }  
   )  
   .catch(function(err) {  
-    console.error('Fetch Error - ', err);  
+    console.error(`Fetch error: ${err}`);  
   });
+
+function convertTemp(temp) {
+  return (temp - 273.15).toFixed(1);    //Convert from Kelvin to Celsius
+}
+
+function getCurrentWeather(cityID) {
+  weatherUrl = `http://api.openweathermap.org/data/2.5/weather?id=3038789&appid=${key}`
+
+  fetch(weatherUrl)
+    .then(function (response) {
+      if (response.status !== 200) {  
+        console.warn(`There was a problem. Status code: ${response.status}`);  
+        return;  
+      }
+
+      response.json().then(function (weatherData) {
+        cityName.innerHTML = weatherData.name;
+        currentWeather.classList.add(`wi-icon-${weatherData.weather[0].id}`);
+        currentTemp.innerHTML = `${convertTemp(weatherData.main.temp)} °C`;
+      });
+    });
+}
+
+getCurrentWeather(524901);
 
